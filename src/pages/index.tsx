4b28e8a95263f24fbf030caf7c7156f0dfb76bf2
0xlny <lenny@419labs.io>
2022-10-21 12:43:20 +0200

    :recycle: Refactor API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,22 @@
 import { Button, Flex, Text, Textarea } from "@chakra-ui/react";
-import { ethers } from "ethers";
 import type { NextPage } from "next";
 import { useState } from "react";
 
-import { fetchAbi } from "../services/api.service";
+import { fetchABI, fetchBytecode } from "../services/api.service";
 
-const FETCH_ABI_ENDPOINT =
-  "https://api-goerli.etherscan.io/api?module=contract&action=getabi&address=";
 const ERC20_KAKAROT_ADDR = "0x3814EaBD9cf0E30e766d7cDaF911af6c56A7A5dA";
 
 const Home: NextPage = () => {
   const [bytecode, setBytecode] = useState<string | undefined>(undefined);
   const [abi, setAbi] = useState<string | undefined>(undefined);
-  /* const fetchABI = () => {
-    fetch(FETCH_ABI_ENDPOINT + ERC20_KAKAROT_ADDR)
-      .then((response) => response.json())
-      .then((data) => {
-        setAbi(data.result);
-      });
-  }; */
-  const fetchBytecode = () => {
-    ethers
-      .getDefaultProvider("goerli")
-      .getCode(ERC20_KAKAROT_ADDR)
-      .then(setBytecode);
-  };
+
   const fetchData = () => {
-    fetchAbi(ERC20_KAKAROT_ADDR, "goerli").then((abi) => {
-      setAbi(abi);
+    fetchABI(ERC20_KAKAROT_ADDR, "goerli").then((result) => {
+      setAbi(result);
     });
-    fetchBytecode();
+    fetchBytecode(ERC20_KAKAROT_ADDR, "goerli").then((result) => {
+      setBytecode(result);
+    });
   };
   return (
     <Flex h="full" direction="column">
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,13 +1,26 @@
+import { ethers } from "ethers";
+
 import environment from "../environment";
 
-export function fetchAbi(contractAddress: string, chain: string) {
+const { etherscanAPIKey } = environment;
+
+export function fetchABI(
+  contractAddress: string,
+  chain: string
+): Promise<string> {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const apiEndpoint = environment.etherscanAPI[chain];
-  const { etherscanAPIKey } = environment;
   return fetch(
     `${apiEndpoint}?module=contract&action=getabi&address=${contractAddress}`
   )
     .then((response) => response.json())
     .then((data) => data.result);
+}
+
+export function fetchBytecode(
+  contractAddress: string,
+  chain: string
+): Promise<string> {
+  return ethers.getDefaultProvider(chain).getCode(contractAddress);
 }
